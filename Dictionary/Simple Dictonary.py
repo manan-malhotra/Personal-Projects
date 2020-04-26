59c2ed1d04ed33176379fe6e1f59b3454c5f58ef
manan-malhotra <60249740+manan-malhotra@users.noreply.github.com>
2020-04-26 11:49:33 +0530

    Update Simple Dictonary.py

--- Dictionary/Simple Dictonary.py
+++ Dictionary/Simple Dictonary.py
@@ -1,12 +1,13 @@
+# Just a simple dictionary using difflib. Enter your word as input and get it's defination as output.
 import json
 from difflib import get_close_matches
-data=json.load(open("data.json"))
+data=json.load(open("data.json"))       #Make sure 'data.json' is in the same directory
 
 def translate(word):
     word=word.lower()
     if word in data:
         return "\n".join(data[word])
-    elif len(get_close_matches(word,data.keys(),cutoff=0.85)) >0:
+    elif len(get_close_matches(word,data.keys(),cutoff=0.85)) >0: #Can change the cutoff to 0.7 for wider results
         yn= input(f"Did you mean {get_close_matches(word,data.keys())[0]} insted? \nPress Y for yes, N for no:")
         if yn.upper()=='Y':
             return "\n".join(data[get_close_matches(word,data.keys())[0]])
